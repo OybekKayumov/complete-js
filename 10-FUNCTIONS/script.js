16ef0d3c797e9408b694d38ab3a7d6f89082a78e
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-16 16:47:55 +0500

    Add functions default parameters

--- 10-FUNCTIONS/script.js
+++ 10-FUNCTIONS/script.js
@@ -1,2 +1,20 @@
 'use strict';
-// Default Parameters
+// Default Parameters (vn128)
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price= 199) {
+  //old way, before ES6
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum, 
+    numPassengers,
+    price
+  }
+
+  console.log(booking);
+  bookings.push(booking);
+}
+
+createBooking('LH123')
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # complete-js
 
+   -command to see projects
+   $live-server
+
 10. Functions
-  -
+  - default parameters
