6a4020c778f1b2a655384a0f97c304dc2077ca48
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-21 15:27:52 +0500

    Add international date

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -81,7 +81,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const formatMovementDate = (date) => {
+const formatMovementDate = (date, locale) => {
   
   const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
@@ -92,12 +92,15 @@ const formatMovementDate = (date) => {
   if (daysPassed === 1) return 'Yesterday';
   if (daysPassed < 7) return `${daysPassed} ago`;
   else {
+    /*
     const day = `${date.getDate()}`.padStart(2, 0); // to get day like 02
     const month = `${date.getMonth() + 1}`.padStart(2, 0);  // (+1) for 0 based
     const year = date.getFullYear();
 
     // const displayDate = `${day}/${month}/${year}`;
     return `${day}/${month}/${year}`;
+    */
+   return Intl.DateTimeFormat(locale).format(date);
   }
 }
 
@@ -110,7 +113,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const displayDate = formatMovementDate(date);
+    const displayDate = formatMovementDate(date, acc.locale);
     
     const html = `
       <div class="movements__row">
@@ -183,6 +186,33 @@ let currentAccount;
 currentAccount = account1;
 updateUI(currentAccount);
 containerApp.style.opacity = 100;
+/*
+// API experiments
+const now = new Date();
+const options = {
+  hour: 'numeric',
+  minute: 'numeric',
+  day: 'numeric',
+  // month: 'numeric',
+  // month: '2-digit',
+  month: 'long',
+  // year: '2-digit',
+  year: 'numeric',
+  // weekday: 'long',
+  // weekday: 'narrow', 
+  weekday: 'short', 
+};
+
+// TODO: get locale from browser
+const locale = navigator.language;
+console.log('locale: ', locale);
+// locale:  ru-RU
+
+// labelDate.textContent = new Intl.DateTimeFormat('en-US').format(now)
+// labelDate.textContent = new Intl.DateTimeFormat('en-GB').format(now)
+// labelDate.textContent = new Intl.DateTimeFormat('ar-SY').format(now)
+labelDate.textContent = new Intl.DateTimeFormat(locale, options).format(now)
+*/
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -201,7 +231,33 @@ btnLogin.addEventListener('click', function (e) {
     containerApp.style.opacity = 100;
 
     // Create current date and time
+    // API experiments
     const now = new Date();
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      // month: 'numeric',
+      // month: '2-digit',
+      month: 'long',
+      // year: '2-digit',
+      year: 'numeric',
+      // weekday: 'long',
+      // weekday: 'narrow', 
+      weekday: 'short', 
+    };
+
+    // TODO: how to get locale from browser
+    const locale = navigator.language;
+    // console.log('locale: ', locale);    
+    // labelDate.textContent = new Intl.DateTimeFormat('en-US').format(now)
+    // labelDate.textContent = new Intl.DateTimeFormat('en-GB').format(now)
+    // labelDate.textContent = new Intl.DateTimeFormat('ar-SY').format(now)
+    // labelDate.textContent = new Intl.DateTimeFormat(locale, options).format(now)
+    labelDate.textContent = new Intl.DateTimeFormat(currentAccount.locale, options).format(now)
+
+    
+    /*
     // labelDate.textContent = now;
     const day = `${now.getDate()}`.padStart(2, 0); // to get day like 02
     const month = `${now.getMonth() + 1}`.padStart(2, 0);  // (+1) for 0 based
@@ -210,6 +266,7 @@ btnLogin.addEventListener('click', function (e) {
     const minutes = `${now.getMinutes()}`.padStart(2, 0);
     labelDate.textContent = `${day}/${month}/${year}, ${hours}:${minutes}`;
     //* day/month/year
+    */
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
