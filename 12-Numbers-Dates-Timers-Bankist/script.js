318bad4d8bb2e0c742340dfbbc139e11890db252
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-21 14:05:09 +0500

    Add day_name to movements date

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -16,14 +16,14 @@ const account1 = {
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2022-12-15T21:31:17.178Z',
+    '2022-12-16T07:42:02.383Z',
+    '2022-12-17T09:15:04.904Z',
+    '2022-12-18T10:17:24.185Z',
+    '2022-12-13T14:11:59.604Z',
+    '2022-12-20T17:01:17.194Z',
+    '2022-12-21T23:36:17.929Z',
+    '2022-12-14T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -81,6 +81,26 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = (date) => {
+  
+  const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log('daysPassed: ', daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed < 7) return `${daysPassed} ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0); // to get day like 02
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);  // (+1) for 0 based
+    const year = date.getFullYear();
+
+    // const displayDate = `${day}/${month}/${year}`;
+    return `${day}/${month}/${year}`;
+  }
+}
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -89,20 +109,15 @@ const displayMovements = function (acc, sort = false) {
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
-    const date = new Date(acc.movementsDates[i])
-
-    const day = `${date.getDate()}`.padStart(2, 0); // to get day like 02
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);  // (+1) for 0 based
-    const year = date.getFullYear();
-
-    const displayDate = `${day}/${month}/${year}`;
-
+    const date = new Date(acc.movementsDates[i]);
+    const displayDate = formatMovementDate(date);
+    
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__date">${displayDate}€</div>
+        <div class="movements__date">${displayDate}</div>
         <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
@@ -526,3 +541,32 @@ console.log(Date.now());
 
 future.setFullYear(2040);
 console.log(future);      // Mon Nov 19 2040 15:34:00 
+
+//TODO: Operations With Dates
+const future2 = new Date(2037, 10, 19, 15, 34);
+console.log(Number.future2 );
+console.log(+future2 );
+
+const calcDaysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24))
+console.log('days1: ', days1);
+// days1:  864000000 milliseconds
+// days1:  10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
