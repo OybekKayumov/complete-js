dfe3f6a89a0a47686f30206d3c31a5f952a16dca
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-26 11:35:50 +0500

    Add App class

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -32,6 +32,13 @@ class App {
 
   constructor() {
     this._getPosition();
+    
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    inputType.addEventListener('change', function () {
+      inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+      inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    });
   }
 
   _getPosition() {
@@ -62,55 +69,44 @@ class App {
     }).addTo(this.#map);    
 
     // handling click on map
-    this.#map.on('click', function (mapE) {
-      this.#mapEvent = mapE;
-      form.classList.remove('hidden');
-      inputDistance.focus();
-  
-    })    
+    this.#map.on('click', this._showForm.bind(this))    
   }
 
-  _showForm() {
-
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
   }
 
   _toggleElevationField() {
 
   }
 
-  _newWorkout() {
+  _newWorkout(e) {
+    e.preventDefault();
 
+    // clear input fields
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
+
+    // display marker
+    const {lat, lng} = this.#mapEvent.latlng;
+        L.marker([lat,lng]).addTo(this.#map)
+          .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+          }))
+          .setPopupContent('Workout!')
+          .openPopup();
   }
 }
 
 const app = new App();
 // app._getPosition();
 
+
 // TODO: Rendering Workout Input Form
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  // clear input fields
-  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
-
-  // display marker
-  const {lat, lng} = mapEvent.latlng;
-      L.marker([lat,lng]).addTo(map)
-        .bindPopup(L.popup({
-          maxWidth: 250,
-          minWidth: 100,
-          autoClose: false,
-          closeOnClick: false,
-          className: 'running-popup'
-        }))
-        .setPopupContent('Workout!')
-        .openPopup();
-});
-
-inputType.addEventListener('change', function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
-
 // TODO: Project Architecture
 // TODO: Refactoring for Project Architecture
