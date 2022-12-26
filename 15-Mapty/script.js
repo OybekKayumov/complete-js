96eeb0f4477250f4419afdf5afb0e1013221bdd3
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-26 07:48:06 +0500

    Add display map marker

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -10,7 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-
+let map, mapEvent;
 
 //---------------------------------------------
 // TODO: How to Plan a Web Project
@@ -37,30 +37,39 @@ if (navigator.geolocation) {
 
     const coords = [latitude, longitude]
 
-    //-----------------------
     // const map = L.map('map').setView([51.505, -0.09], 13);
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     // L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    }).addTo(map);    
 
-    // L.marker([51.5, -0.09]).addTo(map)
-    // L.marker(coords).addTo(map)
-    //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    //     .openPopup();
-
-    map.on('click', function (mapEvent) {
-      const {lat, lng} = mapEvent.latlng;
-
-      L.marker([lat,lng]).addTo(map)
-        .bindPopup('Workout')
-        .openPopup();
+    // handling click on map
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+  
     })
-    //-----------------------
   }, function () {
     // error 
     alert('Could not get your position')
   });
 }
+
+// TODO: Rendering Workout Input Form
+form.addEventListener('submit', function () {
+  // display marker
+  const {lat, lng} = mapEvent.latlng;
+      L.marker([lat,lng]).addTo(map)
+        .bindPopup(L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup'
+        }))
+        .setPopupContent('Workout!')
+        .openPopup();
+})
---
+++ 15-Mapty/temp.md
@@ -0,0 +1,19 @@
+ // L.marker([51.5, -0.09]).addTo(map)
+    // L.marker(coords).addTo(map)
+    //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    //     .openPopup();
+ 
+ /*
+  const {lat, lng} = mapEvent.latlng;
+  L.marker([lat,lng]).addTo(map)
+    .bindPopup(L.popup({
+      maxWidth: 250,
+      minWidth: 100,
+      autoClose: false,
+      closeOnClick: false,
+      className: 'running-popup'
+    }))
+    .setPopupContent('Workout!')
+    .openPopup();
+*/
+
