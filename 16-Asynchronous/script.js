2a623d83672a1606b4c9e433d373eb6e0ebf01b1
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-27 11:14:34 +0500

    Add asynchronous js - get additional data - neigbours

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -107,4 +107,23 @@ const getCountryAndNeighbor = function (country) {
   })
 }
 
-getCountryAndNeighbor('portugal')
+getCountryAndNeighbor('portugal');
+getCountryAndNeighbor('uzb');
+
+
+//
+setTimeout(() => {
+  console.log('1 second passed' );
+  setTimeout(() => {
+    console.log('2 second passed' );
+    setTimeout(() => {
+      console.log('3 second passed' );
+      setTimeout(() => {
+        console.log('4 second passed' );         
+      }, 1000)
+    }, 1000)
+  }, 1000)
+}, 1000)
+
+// TODO: Promises and the Fetch API
+
