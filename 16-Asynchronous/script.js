40c641603907eef5fee39154daaaadede409099b
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-27 09:06:15 +0500

    Add asynchronous js- xmlHttpRequest

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -4,22 +4,40 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-// https://restcountries.com/v2/
-
 // TODO:Our First AJAX Call: XMLHttpRequest
-const req = new XMLHttpRequest();
-req.open('GET', 'https://restcountries.com/v2/name/portugal');
-req.send();
+const getCountryData = function (country) {
+  
+  const req = new XMLHttpRequest();
+  req.open('GET', `https://restcountries.com/v2/name/${country}`);
+  req.send();
+
+  req.addEventListener('load', function () {
+    
+    const [data] = JSON.parse(this.responseText);
+    console.log('data: ', data);
+
+    const html = `
+            <article class="country">
+              <img class="country__img" src="${data.flag}" />
+              <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(2)}</p>
+                <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+                <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+              </div>
+            </article> 
+    `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  })
+}
+
+// https://restcountries.com/v2/
 
 // console.log('this.responseText: ', this.responseText);
 //! this.responseText:  undefined
-
-// 
-req.addEventListener('load', function () {
-  
-  const [data] = JSON.parse(this.responseText);
-  console.log('data: ', data);
-})
 
 // console.log('this.responseText: ', this.responseText);
 // const data = JSON.parse(this.responseText)[0];           //! destructuring
