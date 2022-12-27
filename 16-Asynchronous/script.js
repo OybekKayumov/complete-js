426a0c9f68ee532ba491df20de1bc39cf1a5c5f3
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-27 08:32:24 +0500

    Add asynchronous js- xmlHttpRequest

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -7,3 +7,14 @@ const countriesContainer = document.querySelector('.countries');
 // https://restcountries.com/v2/
 
 // TODO:Our First AJAX Call: XMLHttpRequest
+const req = new XMLHttpRequest();
+req.open('GET', 'https://restcountries.com/v2/name/portugal');
+req.send();
+
+// console.log('this.responseText: ', this.responseText);
+//! this.responseText:  undefined
+
+// 
+req.addEventListener('load', function () {
+  console.log('this.responseText: ', this.responseText);
+})
