3ea155376c444d8f3dc4798ddd7afaf47f9afab2
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-28 11:27:51 +0500

    Add promisifying geolocaion

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -371,9 +371,49 @@ console.log('Getting position');  // 1
 //
 const getPosition = function () {
   return new Promise(function (resolve, reject ) {
-    navigator.geolocation.getCurrentPosition(
-      position => resolve(position),
-      err => reject(err)
-    );    
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );    
+    navigator.geolocation.getCurrentPosition(resolve, reject);
   })
 }
+
+getPosition().then(pos => console.log(pos));
+
+//
+// fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+
+const whereAmI = function () {
+  getPosition().then(pos => {
+    console.log(': ', pos .coords);
+    const {latitude: lat, longitude: lng} = pos.coords;
+
+    return fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
+    )
+  })
+  .then(res => {
+    if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+    return res.json()
+  })
+  .then(data => {
+    console.log('data: ', data);
+    console.log(`You are in ${data.city}, ${data.countryName}`);
+    return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+  })
+  .then(res => {
+    if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+    return res.json();
+  })
+  .then(data => renderCountry(data[0]))  
+  .catch(err => console.error(`${err.message}!`))
+}
+
+// whereAmI();
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
+
+btn.addEventListener('click', whereAmI);
