f4a143e7becf7b593a46abccf89cb13f7de6c446
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-29 07:42:54 +0500

    Add export * fro module

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -3,13 +3,15 @@
 
 //todo: importing module
 // import './shoppingCart.js';
-import { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
+// import { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
+// addToCart('bread', 5);
+// console.log(totalPrice, totalQuantity );
+// console.log(price, tq );
 
 console.log('importing module ');
-
 //! exporting module
 //! importing module 
 
-addToCart('bread', 5);
-// console.log(totalPrice, totalQuantity );
-console.log(price, tq );
+import * as ShoppingCart from './shoppingCart.js';
+// look similar as Object
+ShoppingCart.addToCart('bread', 5)
