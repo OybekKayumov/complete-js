bbfdfaaf7e6deca5f9ed62dbff5c15fad6f09917
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-29 07:29:08 +0500

    Add const to module

--- 17-Modern-JS-Modules-Tooling/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/shoppingCart.js
@@ -1,2 +1,13 @@
 // exporting module
 console.log('exporting module');
+
+const shippingCost = 10;
+const cart = [];
+
+// named and default exports
+const addToCart = function (product, quantity) {
+  cart.push({product, quantity});
+
+  console.log(`${quantity} ${product} added to cart `);
+}
+
