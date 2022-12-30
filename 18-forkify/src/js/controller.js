c5235b0d4b0f08ee183b17b795fbd28d4adf7b64
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-30 15:27:42 +0500

    Add render spinner

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -19,9 +19,25 @@ const timeout = function (s) {
 ///////////////////////////////////////
 // console.log('Test: ', );
 
+const renderSpinner = function (parentEl) {
+  
+  const markup = `
+      <div class="spinner">
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div>
+    `;
+
+    parentEl.innerHTML = '';
+    parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+
 const showRecipe = async function () {
   try {
     // 1 loading recipe
+    renderSpinner(recipeContainer);
+    
     const res = await fetch(
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     );
