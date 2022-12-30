7bda53690391a62071d64029de90daad4367dfb8
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-30 13:02:59 +0500

    Add forkify

---
+++ 18-forkify/src/js/controller.js
@@ -0,0 +1,47 @@
+const { async } = require("regenerator-runtime");
+
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+// console.log('Test: ', );
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`)
+
+    console.log('res, data: ', res, data);
+    let {recipe} = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      imageUrl: recipe.image_url,
+      servings: recipe.servings,
+      cooking_time: recipe.cooking_time,
+      ingredients: recipe.ingredients
+    }
+
+    console.log('recipe: ', recipe);
+  } catch (err) {
+    alert(err)
+  }
+}
+
+showRecipe();
