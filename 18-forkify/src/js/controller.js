82e3fd6c6eac521f48d3a4ee9a838dae3ff1804f
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-30 19:14:29 +0500

    Add id and window

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -38,11 +38,17 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log('id: ', id);
+
+    if(!id) return;
+
     // 1 loading recipe
     renderSpinner(recipeContainer);
 
+    // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
 
     const data = await res.json();
@@ -161,4 +167,8 @@ const showRecipe = async function () {
   }
 }
 
-showRecipe();
+['hashchange', 'load'].forEach(event => window.addEventListener(event, showRecipe));
+
+// showRecipe();
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
