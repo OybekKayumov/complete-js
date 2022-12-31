53faf4d223e953e9f1b9b746febb2d44b03ebadf
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-31 08:32:04 +0500

    Add MVC - views - recipeView

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -40,7 +40,7 @@ const renderSpinner = function (parentEl) {
     parentEl.insertAdjacentHTML('afterbegin', markup);
 };
 
-const showRecipe = async function () {
+const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
     console.log('id: ', id);
@@ -51,110 +51,20 @@ const showRecipe = async function () {
     // 1 loading recipe
     // async fn (loadRecipe) will return Promise, so we should use here await
     await model.loadRecipe(id);
-    const {recipe} = model.state;
+    // const {recipe} = model.state;
     
     // 2 rendering recipe
-    recipeView.render(model.state.recipe);
     // const recipeView = new recipeView(model.state.recipe);
+    recipeView.render(model.state.recipe);
 
-    const markup = `
-        <figure class="recipe__fig">
-              <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
-              <h1 class="recipe__title">
-                <span>${recipe.title}</span>
-              </h1>
-            </figure>
-
-            <div class="recipe__details">
-              <div class="recipe__info">
-                <svg class="recipe__info-icon">
-                  <use href="${icons}#icon-clock"></use>
-                </svg>
-                <span class="recipe__info-data recipe__info-data--minutes">${recipe.cooking_time}</span>
-                <span class="recipe__info-text">minutes</span>
-              </div>
-              <div class="recipe__info">
-                <svg class="recipe__info-icon">
-                  <use href="${icons}#icon-users"></use>
-                </svg>
-                <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
-                <span class="recipe__info-text">servings</span>
-
-                <div class="recipe__info-buttons">
-                  <button class="btn--tiny btn--increase-servings">
-                    <svg>
-                      <use href="${icons}#icon-minus-circle"></use>
-                    </svg>
-                  </button>
-                  <button class="btn--tiny btn--increase-servings">
-                    <svg>
-                      <use href="${icons}#icon-plus-circle"></use>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-
-              <div class="recipe__user-generated">
-                <svg>
-                  <use href="${icons}#icon-user"></use>
-                </svg>
-              </div>
-              <button class="btn--round">
-                <svg class="">
-                  <use href="${icons}#icon-bookmark-fill"></use>
-                </svg>
-              </button>
-            </div>
-
-            <div class="recipe__ingredients">
-              <h2 class="heading--2">Recipe ingredients</h2>
-              <ul class="recipe__ingredient-list">
-
-                ${recipe.ingredients.map(ing => {
-                  return `
-                    <li class="recipe__ingredient">
-                      <svg class="recipe__icon">
-                        <use href="${icons}#icon-check"></use>
-                      </svg>
-                      <div class="recipe__quantity">${ing.quantity}</div>
-                      <div class="recipe__description">
-                        <span class="recipe__unit">${ing.unit}</span>
-                        ${ing.description}
-                      </div>
-                    </li>
-                  `
-                }).join('')}
-            </div>
-
-            <div class="recipe__directions">
-              <h2 class="heading--2">How to cook it</h2>
-              <p class="recipe__directions-text">
-                This recipe was carefully designed and tested by
-                <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
-                directions at their website.
-              </p>
-              <a
-                class="btn--small recipe__btn"
-                href="${recipe.sourceUrl}"
-                target="_blank"
-              >
-                <span>Directions</span>
-                <svg class="search__icon">
-                  <use href="${icons}#icon-arrow-right"></use>
-                </svg>
-              </a>
-            </div>
-    `;
-
-    recipeContainer.innerHTML = '';
-    recipeContainer.insertAdjacentHTML('afterbegin', markup);
+    
   } catch (err) {
     alert(err)
   }
 }
 
-['hashchange', 'load'].forEach(event => window.addEventListener(event, showRecipe));
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipes));
 
 // showRecipe();
-// window.addEventListener('hashchange', showRecipe);
-// window.addEventListener('load', showRecipe);
+// window.addEventListener('hashchange', controlRecipes);
+// window.addEventListener('load', controlRecipes);
