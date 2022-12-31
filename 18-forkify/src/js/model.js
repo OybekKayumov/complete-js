9a5600c9813fab0ef1dcc5865679eb6c7fbe103c
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-31 14:17:17 +0500

    Add search

--- 18-forkify/src/js/model.js
+++ 18-forkify/src/js/model.js
@@ -4,11 +4,15 @@ import { getJSON } from "./helper.js";
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  }
 }
 
 export const loadRecipe = async function (id) {
   try {    
-    const data = await getJSON(`${API_URL}/${id}`)
+    const data = await getJSON(`${API_URL}${id}`)
     
     console.log('res, data: ', res, data);
     const {recipe} = data.data;
@@ -30,3 +34,27 @@ export const loadRecipe = async function (id) {
     throw err;
   }
 }
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log('data: ', data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      }
+    })
+    console.log('state.search.results: ', state.search.results);
+  } catch (err) {
+    console.log('err: ',`${err} 💥💥💥💥`);
+    throw err;
+  }
+}
+
+loadSearchResults('pizza')
