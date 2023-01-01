673350d92a151058c2d716d095e53f3bbb3810a4
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-01 09:52:26 +0500

    Add newDOM

--- 18-forkify/src/js/views/View.js
+++ 18-forkify/src/js/views/View.js
@@ -13,6 +13,18 @@ export default class View {
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
+  update(data) {
+    if(!data || (Array.isArray(data) && data.length === 0)) 
+      return this.renderError();
+
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+  }
+
   _clear() {
     this._parentElement.innerHTML = '';
   }
