37ab410fb3086b6df832636b7df5a19c888e1e21
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-01 11:51:45 +0500

    Add bookmark

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -79,9 +79,15 @@ const controlServings = function (newServings) {
   recipeView.update(model.state.recipe);
 }
 
+const controlAddBookmark = function () {
+  model.addBookmark(model.state.recipe);
+
+}
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
-  recipeView.addHandlerUpdateServings(controlServings);  
+  recipeView.addHandlerUpdateServings(controlServings); 
+  recipeView.addHandlerAddBookmark(controlAddBookmark); 
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
--- 18-forkify/src/js/model.js
+++ 18-forkify/src/js/model.js
@@ -10,6 +10,7 @@ export const state = {
     page: 1,
     resultsPerPage: RES_PER_PAGE,
   },
+  bookmarks: [],
 };
 
 export const loadRecipe = async function (id) {
@@ -52,6 +53,7 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       }
     })
+    state.search.page = 1;
   } catch (err) {
     console.log(`${err} 💥💥💥💥`);
     throw err;
@@ -75,3 +77,11 @@ export const updateServings = function (newServings) {
 
   state.recipe.servings = newServings;
 };
+
+export const addBookmark = function (recipe) {
+  //add bookmark
+  state.bookmarks.push(recipe);
+
+  // mark current recipe as bookmark
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+}
