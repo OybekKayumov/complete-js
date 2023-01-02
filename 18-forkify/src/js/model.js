5a1e6350581a045483e0098aa5c877c1152cf4d9
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-02 08:02:43 +0500

    Add ajax

--- 18-forkify/src/js/model.js
+++ 18-forkify/src/js/model.js
@@ -1,6 +1,7 @@
 import { async } from "regenerator-runtime";
 import { API_URL, RES_PER_PAGE, KEY } from "./config.js";
-import { getJSON, sendJSON } from "./helper.js";
+// import { getJSON, sendJSON } from "./helper.js";
+import { AJAX } from "./helper.js";
 
 export const state = {
   recipe: {},
