bbe772b730579e46d8f4df1d473cdc2e9a4afacb
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-26 17:17:00 +0500

    Add repeat- js fundamentals part1

---
+++ 02-JS-fundamentals-p1/index.js
@@ -0,0 +1,16 @@
+// JS is a high-level, object-oriented, multi-paradigm programming language
+// high-level - which means that we don't have to think how to managing the computer's memory while it runs or program, there are a lot of so-called abstractions over all these small details that we don't want to worry about. And this makes the language a lot easier to write and to learn.
+
+// multi-paradigm -  we can use all kinds of different programming styles, such as imperative and declarative programming
+
+// HTML - CSS - JS
+// Content - Presentation - Building web applications
+// Nouns - Adjectives - Verbs
+
+// JS - dynamic effects and web apps in the browser, Front-end
+   // - web-app on web-servers                     , Back-end
+
+
+
+
+
